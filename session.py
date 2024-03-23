ff4501a903b6d00a6a005a36a936deb18e51831c
Mahdi Mashal <mhdml1379@gmail.com>
2024-03-24 01:26:18 +0330

    added create method from database query result

--- session.py
+++ session.py
@@ -7,3 +7,17 @@ class Session:
         self.start_time = start_time
         self.end_time = end_time
         self.created_at = created_at or datetime.now()
+
+    # converts query fetch results to session object(s)
+    @staticmethod
+    def create(fetch_result):
+        if fetch_result:
+            if isinstance(fetch_result[0], tuple):
+                # If fetch_result is a single tuple, return a single Session object
+                return Session(*fetch_result[0])
+            else:
+                # If fetch_result is a list of tuples, return a list of Session objects
+                return [Session(*row) for row in fetch_result]
+        else:
+            return None
+
